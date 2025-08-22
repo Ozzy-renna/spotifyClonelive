c3e56016dce5c8890e8e214c09cfdbf38d149d2e
Ozzy-renna <okerekeozzy123@gmail.com>
2025-08-22 12:37:52 +0100

    Prepare SpotifyClonelive for GitHub Pages

---
+++ script.js
@@ -0,0 +1,21 @@
+let isPlaying = false;
+
+function togglePlay() {
+    const btn = document.getElementById("playBtn");
+    const bar = document.querySelector(".filled");
+
+    if (isPlaying) {
+        btn.src = "play-button.png";   // show play
+        bar.style.animationPlayState = "paused"; 
+        isPlaying = false;
+    } else {
+       if(bar.offsetWidth===bar.parentElement.offsetWidth) {
+        bar.style.animation="none";
+        bar.offsetHeight;
+        bar.style.animation=null;
+       }
+       btn.src="pause.png";
+       bar.style.animationPlayState="running";
+       isPlaying=true;
+    }
+}
